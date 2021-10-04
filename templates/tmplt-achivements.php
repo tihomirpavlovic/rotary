efd3fd15d2ba01e52fcfb50b55f6eba2bc8e1af5
Tihomir Pavlovic <tihomir.pavlovic1112@gmail.com>
2021-10-04 12:29:53 +0200

    achivements backend

--- templates/tmplt-achivements.php
+++ templates/tmplt-achivements.php
@@ -1,5 +1,9 @@
 <?php
 /* Template Name: Achivements */
+
+$texte_introduction = get_field('texte_introduction');
+$projets = get_field('projets');
+
 get_header(); ?>
     <div class="template_achivements_wrap">
         <div class="global_hero">
@@ -9,72 +13,43 @@ get_header(); ?>
             </div>
             <div class="hero_headline_wrap">
                 <h2 class="hero_headline">
-                    Principales <br> réalisations
+                    <?php echo get_the_title(); ?>
+                    <!-- Principales <br> réalisations -->
                 </h2>
             </div>
         </div>
 
         <div class="template_achivements_content content_wrap">
-            <div class="page_hero_descripton">
-                <h2>Mise en place et réalisation de projets</h2>
-                <p>
-                    Praesent id suscipit tortor, id dictum arcu. Donec venenatis, massa nec tempus rutrum, augue neque tempus neque, eget auctor magna felis a elit. Quisque ac finibus lorem, et maximus magna. Sed interdum, augue id posuere interdum, quam orci lacinia eros, sit amet feugiat dui justo eget mi. Etiam at justo ut libero feugiat elementum. Phasellus porta, tellus ac egestas sodales, felis ligula sodales leo, ac maximus mi mauris nec libero. Curabitur a rutrum nulla, ac fermentum lorem. Etiam viverra vehicula ligula sed ornare. Integer non porttitor tellus. Ut tristique eros sit amet libero pellentesque, iaculis egestas nibh iaculis.
-                </p>
-            </div>
-        
-            <div class="achivements_list">
-                <div class="single_item">
-                    <div class="left">
-                        <div class="image_holder">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/dev/fondation-olo.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="right">
-                        <h3>La Petite Ligue de Baseball de Valleyfield</h3>
-                        <p>
-                            Notre action : Curabitur eget consectetur sapien, vitae gravida orci. Duis commodo, sapien id tincidunt scelerisque, metus mauris aliquam massa, ut aliquet eros orci nec nibh. Quisque sed sem sed lectus dignissim suscipit. Donec rutrum suscipit sapien quis gravida. Fusce id urna magna.
-                        </p>
-                        <a href="">
-                            Site web
-                        </a>
-                    </div>
-                    
+            <?php if($texte_introduction): ?>
+                <div class="page_hero_descripton">
+                    <?php echo $texte_introduction; ?>
                 </div>
-                <div class="single_item">
-                    <div class="left">
-                        <div class="image_holder">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/dev/fondation-olo.jpg" alt="">
+            <?php endif; ?>
+            <?php if($projets): ?>
+                <div class="achivements_list">
+                    <?php foreach ($projets as $singleProjects): ?>
+                        <div class="single_item">
+                            <div class="left">
+                                <?php if($singleProjects['image']): ?>
+                                    <div class="image_holder">
+                                        <img src="<?php echo $singleProjects['image']['url'] ?>" alt="<?php echo $singleProjects['image']['alt'] ?>">
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <div class="right">
+                                <?php if($singleProjects['texte']): ?>
+                                    <?php echo $singleProjects['texte'] ?>
+                                <?php endif; ?>
+                                <?php if($singleProjects['site_web']): ?>
+                                    <a href="<?php echo $singleProjects['site_web'] ?>">
+                                        Site web
+                                    </a>
+                                <?php endif; ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="right">
-                        <h3>La Petite Ligue de Baseball de Valleyfield</h3>
-                        <p>
-                            Notre action : Curabitur eget consectetur sapien, vitae gravida orci. Duis commodo, sapien id tincidunt scelerisque, metus mauris aliquam massa, ut aliquet eros orci nec nibh. Quisque sed sem sed lectus dignissim suscipit. Donec rutrum suscipit sapien quis gravida. Fusce id urna magna.
-                        </p>
-                        <a href="">
-                            Site web
-                        </a>
-                    </div>
-                    
+                    <?php endforeach ?>
                 </div>
-                <div class="single_item">
-                    <div class="left">
-                        <div class="image_holder">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/dev/fondation-olo.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="right">
-                        <h3>La Petite Ligue de Baseball de Valleyfield</h3>
-                        <p>
-                            Notre action : Curabitur eget consectetur sapien, vitae gravida orci. Duis commodo, sapien id tincidunt scelerisque, metus mauris aliquam massa, ut aliquet eros orci nec nibh. Quisque sed sem sed lectus dignissim suscipit. Donec rutrum suscipit sapien quis gravida. Fusce id urna magna.
-                        </p>
-                        <a href="">
-                            Site web
-                        </a>
-                    </div>
-                    
-                </div>
-            </div>
+            <?php endif; ?>
         </div>
     </div>
 <?php
